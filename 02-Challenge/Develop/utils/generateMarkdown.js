8f95dbc36e8466178acf0aebd659e831eace7685
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-05-15 19:55:21 -0600

    returns

--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -1,10 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+  } else if (license === "Apache") {
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+  } else if (license === "GPL") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+  } else if (license === "BSD") {
+    return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]";
+  } else if (license === "None") {
+    return "";
+  }
+}
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
+if (license === "MIT") {
+  return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+}
+if (license === "Apache") {
+  return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+}
+if (license === "GPL") {
+  return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+}
+if (license === "BSD") {
+  return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]";
+}
+if (license === "None") {
+  return "";
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
