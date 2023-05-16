637718632860d0dc86a718471146fe8c316df1c0
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-05-15 19:56:29 -0600

    functions

--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -34,6 +34,23 @@ if (license === "None") {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+function renderLicenseLink(license) {}
+if (license === "MIT") {
+  return "(https://opensource.org/licenses/MIT)";
+}
+if (license === "Apache") {
+  return "(https://opensource.org/licenses/Apache-2.0)";
+}
+if (license === "GPL") {
+  return "(https://www.gnu.org/licenses/gpl-3.0)";
+}
+if (license === "BSD") {
+  return "(https://opensource.org/licenses/BSD-3-Clause)";
+}
+if (license === "None") {
+  return "";
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
